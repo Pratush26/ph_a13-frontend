43d83b508721aa24a02ee2271e0e59b9c8a879c4
Pratush26 <pratushbarua26@gmail.com>
2026-01-12 20:26:06 +0600

    Update frontend code

---
+++ src/Routes/Routes.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router";
+import HomeLayout from "../Layouts/Home";
+import HomePage from "../Pages/Home";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        Component: HomeLayout,
+        // hydrateFallbackElement: <LoadingUi />,
+        // errorElement: <ErrorPage />,
+        children: [
+            {
+                index: true,
+                Component: HomePage
+            },
+        ]
+    },
+])
